26a581deb7a6f307043cc91bef24dcd1669469da
DESKTOP-Q7R713D\Vadym <vadim.nakitnyak@gmail.com>
2021-03-04 14:20:33 +0200

    Added SignIn component

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,11 @@
 import React from 'react';
+import SignIn from "./SignIn";
 
-import logo from '../resources/images/logo.png';
 import '../styles/App.sass';
 
 function App() {
     return (
-        <div>
-        <img src={logo}/>
-        <p>
-            Hello World !!!
-        </p>
-        </div>
+        <SignIn/>
     );
 }
 
