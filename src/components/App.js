93be8f4cae263789186cf849b00d270fe519106d
DESKTOP-Q7R713D\Vadym <vadim.nakitnyak@gmail.com>
2021-03-04 13:57:00 +0200

    Config changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,16 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
 
 import logo from '../resources/images/logo.png';
-import '../styles/App.scss';
+import '../styles/App.sass';
 
 function App() {
     return (
-        <body>
+        <div>
         <img src={logo}/>
-        <Button variant="contained" color="primary">
-            Hello World
-        </Button>
         <p>
             Hello World !!!
         </p>
-        </body>
+        </div>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import './styles/App.scss';
+import './styles/index.sass';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
